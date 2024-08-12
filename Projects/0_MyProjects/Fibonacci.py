8963a755e1551dd311e74d1bbe625d1dca8379b0
Mert USTUNER <mrt.ustnr@gmail.com>
2024-08-12 22:50:16 +0300

    python

---
+++ Projects/0_MyProjects/Fibonacci.py
@@ -0,0 +1,16 @@
+print("Fibonacci Dizisi Oluşturma Programına Hoş Geldiniz!!!")
+
+sayi = int(input("Kaç elemanlı Fibonacci dizisi oluşturmak istediğinizi girin:"))
+
+if sayi <= 0:
+    print("Lütfen pozitif bir sayı giriniz.")
+else:
+    # Fibonacci dizisini oluştur
+    liste = [0, 1]
+    while len(liste) < sayi:
+        liste.append(liste[-1] + liste[-2])
+
+    # İstenilen eleman sayısından fazla olursa kes
+    liste = liste[:sayi]
+
+    print(f"İlk {sayi} Fibonacci sayısı: {liste}")
